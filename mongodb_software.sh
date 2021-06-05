4da5d2dc5d7911aaee03b775b7945f9fa2f5ec40
mallelatulasiram <82141959+mallelatulasiram@users.noreply.github.com>
2021-06-05 17:01:11 +0530

    Update mongodb_software.sh

--- mongodb_software.sh
+++ mongodb_software.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #install mongodb
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-apt-key list
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt update
+#curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+#apt-key list
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+#sudo apt update
 #sudo apt install mongodb-org
 #sudo systemctl start mongod.service
 #sudo systemctl status mongod
+echo "thulasiram is a great person"
