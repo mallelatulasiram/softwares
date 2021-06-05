5e8d5ccd7235439edd899ac45c42b4eb0d255338
mallelatulasiram <82141959+mallelatulasiram@users.noreply.github.com>
2021-06-05 14:31:26 +0530

    Create mongodb_software.sh

---
+++ mongodb_software.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#install mongodb
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+apt-key list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install mongodb-org
+sudo systemctl start mongod.service
+sudo systemctl status mongod
+sudo systemctl enable mongod
+mongo --eval 'db.runCommand({ connectionStatus: 1 })'
+sudo systemctl status mongod
