8c3463afd0fdaff8e7b6d12de93e16f112963fcf
mallelatulasiram <82141959+mallelatulasiram@users.noreply.github.com>
2021-06-05 16:57:53 +0530

    Update mongodb_software.sh

--- mongodb_software.sh
+++ mongodb_software.sh
@@ -5,6 +5,6 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add 
 apt-key list
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt update
-sudo apt install mongodb-org
-sudo systemctl start mongod.service
-sudo systemctl status mongod
+#sudo apt install mongodb-org
+#sudo systemctl start mongod.service
+#sudo systemctl status mongod
